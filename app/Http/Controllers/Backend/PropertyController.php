45b2185547c1a55185cbce3c7c775e26067b4a61
uusa35 <uusa35@gmail.com>
2020-11-19 13:59:21 +0300

    updates

--- app/Http/Controllers/Backend/PropertyController.php
+++ app/Http/Controllers/Backend/PropertyController.php
@@ -42,12 +42,12 @@ class PropertyController extends Controller
         }
         $element = Classified::whereId($request->id)->first();
         foreach ($request->properties as $k => $v) {
-            $element->properties()->sync([
-                $element->id => [
-                    'property_id' => $k,
-                    'value' => $v,
-                    'category_group_id' => $request->category_group_id[$k]
-                ]]);
+            $element->properties()->create([
+                'classified_id' => $element->id,
+                'property_id' => $k,
+                'value' => $v,
+                'category_group_id' => $request->category_group_id[$k]
+            ]);
         }
         return redirect()->route('backend.classified.index')->with('success', 'classified properties saved successfully');
     }
