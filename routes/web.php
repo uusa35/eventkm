7ec1f9a6597aad445779184e20c6727dfd9d97a7
uusa35 <uusa35@gmail.com>
2020-12-23 11:26:34 +0300

    updates

--- routes/web.php
+++ routes/web.php
@@ -119,55 +119,7 @@ Route::group(['namespace' => 'Backend', 'prefix' => 'backend', 'as' => 'backend.
 });
 
 Route::group(['namespace' => 'Frontend', 'as' => 'frontend.', 'middleware' => ['country']], function () {
-    dd(env('ABATI'));
-    if (env('ABATI')) {
-        Route::get('/', 'HomeController@getMobileLayout')->name('index');
-        Route::get('/home', 'HomeController@getMobileLayout')->name('home');
-    } elseif (env('MALLR')) {
-        Route::get('/', 'HomeController@getMallrHome')->name('index');
-        Route::get('/home', 'HomeController@getMallrHome')->name('home');
-    } elseif (env('HOMEKEY')) {
-        Route::get('/', 'HomeController@getHomekeyHome')->name('index');
-        Route::get('/home', 'HomeController@getHomekeyHome')->name('home');
-//        Route::get('/', 'HomeController@getMobileLayout')->name('index');
-//        Route::get('/home', 'HomeController@getMobileLayout')->name('home');
-    } elseif (env('ESCRAP')) {
-        Route::get('/', 'HomeController@getMobileLayout')->name('index');
-        Route::get('/home', 'HomeController@getMobileLayout')->name('home');
-    } elseif (env('ABATI')) {
-        Route::get('/', 'HomeController@getMobileLayout')->name('index');
-        Route::get('/home', 'HomeController@getMobileLayout')->name('home');
-    } elseif (env('EVENTKM')) {
-        Route::get('/', 'HomeController@getEventKmHome')->name('index');
-        Route::get('/home', 'HomeController@getEventKmHome')->name('home');
-    } elseif (env('BITS')) {
-        Route::get('/', 'HomeController@getMallrHome')->name('index');
-        Route::get('/home', 'HomeController@getMallrHome')->name('home');
-    } elseif (env('TOYS')) {
-        Route::get('/', 'HomeController@getMallrHome')->name('index');
-        Route::get('/home', 'HomeController@getMallrHome')->name('home');
-    } elseif (env('DAILY')) {
-        Route::get('/', 'HomeController@getDailyHome')->name('index');
-        Route::get('/home', 'HomeController@getDailyHome')->name('home');
-    } elseif (env('ATSPOT')) {
-        Route::get('/', 'HomeController@getMobileLayout')->name('index');
-        Route::get('/home', 'HomeController@getMobileLayout')->name('home');
-    } elseif (env('NASHKW')) {
-        Route::get('/', 'HomeController@getNashKwHome')->name('index');
-        Route::get('/home', 'HomeController@getNashKwHome')->name('home');
-    } elseif (env('EMAKEUP')) {
-        Route::get('/', 'HomeController@getEmakeupHome')->name('index');
-        Route::get('/home', 'HomeController@getEmakeupHome')->name('home');
-    } elseif (env('EXPO')) {
-        Route::get('/', 'HomeController@getMobileLayout')->name('index');
-        Route::get('/home', 'HomeController@getMobileLayout')->name('home');
-    } elseif (env('HTB')) {
-        Route::get('/', 'HomeController@getMallrHome')->name('index');
-        Route::get('/home', 'HomeController@getMallrHome')->name('home');
-    } elseif (env('HUDA')) {
-        Route::get('/', 'HomeController@getMallrHome')->name('index');
-        Route::get('/home', 'HomeController@getMallrHome')->name('home');
-    }
+    include 'home.php';
     Route::group(['middleware' => ['auth']], function () {
         Route::get('favorite', 'FavoriteController@index')->name('favorite.index');
         Route::get('favorite/add/product/{id}', 'FavoriteController@addProduct')->name('favorite.product.add');
