16b0df46809d92cd7bc2941ae2ab719173d90ece
uusa35 <uusa35@gmail.com>
2020-12-23 11:10:59 +0300

    updates

--- resources/views/backend/modules/order/show.blade.php
+++ resources/views/backend/modules/order/show.blade.php
@@ -91,7 +91,7 @@
                         <tbody>
                         @foreach($element->order_metas as $item)
                             @if($item->isProductType)
-                                @if(!is_null($item->product->product_attributes) && $item->product->has_attributes)
+                                @if($item->product->product_attributes->isNotEmpty() && $item->product->has_attributes)
                                     <tr>
                                         <td class="center">{{ $item->product_id }}</td>
                                         <td class="center">{{ $item->product->sku }}</td>
