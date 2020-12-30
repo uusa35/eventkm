09377500988df6acfec413034009c1cb8b5fb99c
uusa35 <uusa35@gmail.com>
2020-12-30 15:05:02 +0300

    updates

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:80', 'min:5'],
+            'name' => ['required', 'string', 'max:80', 'min:3'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users','min:8'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
             'mobile' => 'string|max:10|min:5',
