8233a7c2515bd1d3cf8eab264e4adbce03aa2009
uusa35 <uusa35@gmail.com>
2021-02-01 09:42:57 +0300

    updates

--- resources/views/frontend/wokiee/four/partials/_shipment_and_size_chart_btns.blade.php
+++ resources/views/frontend/wokiee/four/partials/_shipment_and_size_chart_btns.blade.php
@@ -1,16 +1,20 @@
 <div class="tt-wrapper product-information-buttons text-center">
-    <a data-toggle="modal" data-target="#modalProductInfo" href="#">
+    @if($element->size_chart_image)
+        <a data-toggle="modal" data-target="#modalProductInfo" href="#">
                                         <span class="align-content-center">
                                             <i class="fa fa-fw fa-lg icon-e-75"></i>
                                             <span>
                                         {{ trans('general.size_guide') }}
                                             </span>
                                         </span>
-    </a>
-    <a data-toggle="modal" data-target="#modalProductInfo-02" href="#">
+        </a>
+    @endif
+    @if(count(trans('general.shipping')) > 60 )
+        <a data-toggle="modal" data-target="#modalProductInfo-02" href="#">
                                         <span>
                                             <i class="fa fa-fw fa-lg icon-f-48"></i>
                                         {{ trans('general.shipping') }}
                                         </span>
-    </a>
+        </a>
+    @endif
 </div>
