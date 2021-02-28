e9dced24aa2511da0565e61ac8773d4deb62e73c
uusa35 <uusa35@gmail.com>
2021-02-28 15:11:39 +0300

    updates

--- resources/views/backend/modules/country/create.blade.php
+++ resources/views/backend/modules/country/create.blade.php
@@ -126,7 +126,7 @@
                                         <div class="col-md-6">
                                             <div class="form-group">
                                                 <label class="control-label">{{ trans('general.flag_image') }} </label>
-                                                <input id="flag" type="file" class="form-control" name="image">
+                                                <input id="flag" type="file" class="form-control" name="image" required>
                                                 <span class="help-block"> flag size is 400 X 250</span>
                                             </div>
                                         </div>
