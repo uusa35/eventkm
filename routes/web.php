38f28be1726f2902b83dca1446c8330b057ab6ae
uusa35 <uusa35@gmail.com>
2021-03-09 15:53:59 +0300

    updates

--- routes/web.php
+++ routes/web.php
@@ -99,9 +99,9 @@ Route::group(['namespace' => 'Backend', 'prefix' => 'backend', 'as' => 'backend.
     Route::resource('classified', 'ClassifiedController');
     Route::get('trashed/classified', 'ClassifiedController@trashed')->name('classified.trashed');
     Route::get('restore/classified/{id}', 'ClassifiedController@restore')->name('classified.restore');
-    Route::get('property/attach', 'PropertyController@getAttach')->name('property.attach');
-    Route::post('property/attach', 'PropertyController@postAttach')->name('property.attach');
-    Route::get('property/dettach', 'PropertyController@detach')->name('property.detach');
+//    Route::get('property/attach', 'PropertyController@getAttach')->name('property.attach');
+//    Route::post('property/attach', 'PropertyController@postAttach')->name('property.attach');
+//    Route::get('property/dettach', 'PropertyController@detach')->name('property.detach');
     Route::resource('slide', 'SlideController');
     Route::get('trashed/slide', 'SlideController@trashed')->name('slide.trashed');
     Route::get('restore/slide/{id}', 'SlideController@restore')->name('slide.restore');
