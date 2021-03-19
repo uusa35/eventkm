47fba3818d7c15574917dbd10d69e8fec59f7d95
uusa35 <uusa35@gmail.com>
2021-03-19 09:54:06 +0300

    updates

--- app/Services/CartTrait.php
+++ app/Services/CartTrait.php
@@ -55,7 +55,7 @@ trait CartTrait
         }
         $settings = Setting::first();
         if ($settings->shipment_fixed_rate) {
-            $this->cart->add($country->calling_code, trans('shipment_package_fee'), $this->cart->count() < 1 || $country->is_local ? 1 : $this->getTotalItemsOnly($cart), (double)$country->fixed_shipment_charge, 1, ['type' => 'country', 'country_id' => $country->id]);
+            $this->cart->add($country->calling_code, trans('shipment_package_fee'), $country->is_local ? 1 : $this->getTotalItemsOnly($cart), (double)$country->fixed_shipment_charge, 1, ['type' => 'country', 'country_id' => $country->id]);
         } else {
             $shipmentPackage = $country->shipment_packages()->first();
             $totalWeight = $this->cart->content()->sum('weight');
