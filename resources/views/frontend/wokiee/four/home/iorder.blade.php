892d5931b045053a97bbaeecd501143a31bc1560
uusa35 <uusa35@gmail.com>
2021-04-25 14:51:45 +0300

    updates

--- resources/views/frontend/wokiee/four/home/iorder.blade.php
+++ resources/views/frontend/wokiee/four/home/iorder.blade.php
@@ -3,7 +3,7 @@
 @section('body')
     @include('frontend.wokiee.four.partials.slider')
     @include('frontend.wokiee.four.partials._all_brands', ['elements' => $brands])
-    @include('frontend.wokiee.four.partials._home_grid_main_categories', ['title' => trans('general.product_categories')])
+
 {{--    @include('frontend.wokiee.four.partials.category._categories_slider_full_width')--}}
     @include('frontend.wokiee.four.partials.category._categories_slider_full_width', ['title' => trans('general.company_categories'), 'elements' => $categoriesHome])
 {{--    @include('frontend.wokiee.four.partials._products_metro_collection',['element' => $bestSaleCollections->first(), 'title' => trans('general.our_selection_from_collections')])--}}
@@ -11,6 +11,7 @@
 {{--    @include('frontend.wokiee.four.partials._collection_slider_with_cover',['title' => trans('general.our_selection_from_collections'), 'elements' => $bestSaleCollections])--}}
 {{--    @include('frontend.wokiee.four.partials._users_slider',['title' => trans('general.our_personal_shoppers'), 'elements' => $designers])--}}
     @include('frontend.wokiee.four.partials._users_slider',['title' => trans('general.some_companies'), 'elements' => $companies])
+    @include('frontend.wokiee.four.partials._home_grid_main_categories', ['title' => trans('general.product_categories')])
     @include('frontend.wokiee.four.partials._products_slider',['title' => trans('general.new_arrivals'), 'elements' => $newProducts])
     @include('frontend.wokiee.four.partials._products_slider',['title' => trans('general.on_sale_products'), 'elements' => $onSaleProducts])
     @if(isset($categoriesHome) && $categoriesHome->isNotEmpty())
