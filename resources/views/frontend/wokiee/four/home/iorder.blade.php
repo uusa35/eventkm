0dba1ba980dd365e9494e610bee09e50e5d13239
uusa35 <uusa35@gmail.com>
2021-04-25 16:15:49 +0300

    updates

--- resources/views/frontend/wokiee/four/home/iorder.blade.php
+++ resources/views/frontend/wokiee/four/home/iorder.blade.php
@@ -11,16 +11,17 @@
 {{--    @include('frontend.wokiee.four.partials._collection_slider_with_cover',['title' => trans('general.our_selection_from_collections'), 'elements' => $bestSaleCollections])--}}
 {{--    @include('frontend.wokiee.four.partials._users_slider',['title' => trans('general.our_personal_shoppers'), 'elements' => $designers])--}}
     @include('frontend.wokiee.four.partials._users_slider',['title' => trans('general.some_companies'), 'elements' => $companies])
+    @include('frontend.wokiee.four.partials._users_slider',['title' => trans('general.small_business'), 'elements' => $companies])
     @include('frontend.wokiee.four.partials._home_grid_main_categories', ['title' => trans('general.product_categories')])
     @include('frontend.wokiee.four.partials._products_slider',['title' => trans('general.new_arrivals'), 'elements' => $newProducts])
     @include('frontend.wokiee.four.partials._products_slider',['title' => trans('general.on_sale_products'), 'elements' => $onSaleProducts])
-    @if(isset($categoriesHome) && $categoriesHome->isNotEmpty())
+{{--    @if(isset($categoriesHome) && $categoriesHome->isNotEmpty())--}}
         @include('frontend.wokiee.four.partials._five_categories',['elements' => $categoriesHome])
-    @endif
+{{--    @endif--}}
     {{--        @if(isset($tripleCommercials) && $tripleCommercials->isNotEmpty())--}}
     {{--            @include('frontend.wokiee.four.partials._horizontal_three_categories',['elements' => $tripleCommercials])--}}
     {{--        @endif--}}
-    @include('frontend.wokiee.four.partials._products_slider_hot_deal', ['elements' => $productHotDeals,'items' => 3])
+{{--    @include('frontend.wokiee.four.partials._products_slider_hot_deal', ['elements' => $productHotDeals,'items' => 3])--}}
     @include('frontend.wokiee.four.partials._btn_info')
     @include('frontend.wokiee.four.partials._country_modal')
 @endsection
