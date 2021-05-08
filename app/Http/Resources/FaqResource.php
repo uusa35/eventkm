8d772c2bc412d33c0a77fdd863f195b5a37e96ed
uusa35 <uusa35@gmail.com>
2021-05-08 13:26:19 +0300

    updates

--- app/Http/Resources/FaqResource.php
+++ app/Http/Resources/FaqResource.php
@@ -14,6 +14,11 @@ class FaqResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+//        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'content' => $this->content
+        ];
     }
 }
