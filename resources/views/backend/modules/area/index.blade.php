84f97e699808d0c511bc4fa36d2f6bdc8cec1bd9
uusa35 <uusa35@gmail.com>
2021-06-20 16:04:45 +0300

    updates

--- resources/views/backend/modules/area/index.blade.php
+++ resources/views/backend/modules/area/index.blade.php
@@ -56,6 +56,11 @@
                                                 <i class="fa fa-fw fa-edit"></i> {{trans('general.edit')}}</a>
                                         </li>
                                         <li>
+                                            <a href="{{ route('backend.activate',['model' => 'area','id' => $element->id]) }}">
+                                                <i class="fa fa-fw fa-check-circle"></i> {{ trans('general.toggle_active') }}
+                                            </a>
+                                        </li>
+                                        <li>
                                             <a data-toggle="modal" href="#" data-target="#basic" data-title="Delete" data-content="Are you sure you want to delete term {{ $element->name }}? " data-form_id="delete-{{ $element->id }}">
                                                 <i class="fa fa-fw fa-recycle"></i> {{trans('general.delete')}}</a>
                                             <form method="post" id="delete-{{ $element->id }}" action="{{ route('backend.admin.area.destroy',$element->id) }}">
