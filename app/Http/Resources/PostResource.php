5db84e107b8a473e395ca5f4aec47676f0f7bd9b
uusa35 <uusa35@gmail.com>
2021-06-21 11:15:22 +0300

    updates

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -14,6 +14,17 @@ class PostResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'thumb' => $this->getCurrentImageAttribute(),
+            'large' => $this->getCurrentImageAttribute('image', 'large'),
+            'title' => $this->title,
+            'slug' => $this-> slug,
+            'content' => $this-> content,
+            'keywords' => $this->keywords,
+            'video_url' => $this->video_url,
+            'user' => new UserExtraLightResource($this->whenLoaded('user')),
+            'comments' => CommentExtraLightResource::collection($this->whenLoaded('comments')),
+        ];
     }
 }
