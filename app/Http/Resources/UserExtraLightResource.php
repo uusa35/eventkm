9a13d1e71609216cdd47586ec5520d373857eb74
uusa35 <uusa35@gmail.com>
2021-06-29 09:59:39 +0300

    updates

--- app/Http/Resources/UserExtraLightResource.php
+++ app/Http/Resources/UserExtraLightResource.php
@@ -18,7 +18,11 @@ class UserExtraLightResource extends JsonResource
         return [
             'id' => $this->id,
             'slug' => ucfirst(Str::limit($this->slug ? $this->slug : $this->name, 30, '')),
+            'slug_ar' => ucfirst(Str::limit($this->slug_ar ? $this->slug_ar : $this->name_ar, 30, '')),
+            'slug_en' => ucfirst(Str::limit($this->slug_en ? $this->slug_en : $this->name_en, 30, '')),
             'description' => $this->description ? ucfirst(Str::limit($this->description, 80, '')) : ucfirst(Str::limit($this->service, 80, '')),
+            'description_ar' => $this->description_ar ? ucfirst(Str::limit($this->description_ar, 80, '')) : ucfirst(Str::limit($this->service_ar, 80, '')),
+            'description_en' => $this->description_en ? ucfirst(Str::limit($this->description_en, 80, '')) : ucfirst(Str::limit($this->service_en, 80, '')),
             'thumb' => $this->getCurrentImageAttribute('image'),
             'fullMobile' => $this->fullMobile,
             'whatsapp' => $this->fullWhatsapp,
