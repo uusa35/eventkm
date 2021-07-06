41a8f018775b7130f5f8eaa7e639d71e4f27623f
uusa35 <uusa35@gmail.com>
2021-07-06 08:45:24 +0300

    updates

--- app/Http/Resources/FaqResource.php
+++ app/Http/Resources/FaqResource.php
@@ -18,7 +18,11 @@ class FaqResource extends JsonResource
         return [
             'id' => $this->id,
             'title' => $this->title,
-            'content' => trim(filter_var(strip_tags($this->content), FILTER_SANITIZE_STRING))
+            'title_ar' => $this->title_ar,
+            'title_en' => $this->title_en,
+            'content' => trim(filter_var(strip_tags($this->content), FILTER_SANITIZE_STRING)),
+            'content_ar' => trim(filter_var(strip_tags($this->content_ar), FILTER_SANITIZE_STRING)),
+            'content_en' => trim(filter_var(strip_tags($this->content_en), FILTER_SANITIZE_STRING))
         ];
     }
 }
